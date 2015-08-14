2a86bb27b14c313fed8960ffa7ee340d07b32b05
Uri ZACKHEM <urizackhem@gmail.com>
2015-08-14 20:10:24 +0300

    Query the PWS from Weather Undeground
    Query the PWS from Weather Undeground and get the data as JSON for
    further usage.

---
+++ GetMyWeather.py
@@ -0,0 +1,36 @@
+import time
+from datetime import datetime
+import urllib2
+import json
+'''
+After obtaining a <WUNDERGROUND API KEY> from Weather underground (there are free plans)
+You can query your Personal weather station (PWS) by using the <PWS ID>
+and get the data in Json format for further usage.
+'''
+myurl        = 'http://api.wunderground.com/api/<WUNDERGROUND API KEY>/conditions/settings/q/pws:<PWS ID>.json'
+outurlprefix = 'http://wow.metoffice.gov.uk/automaticreading?'
+
+def get_report():
+    rettup = []
+    try:
+        page = urllib2.urlopen(myurl)
+        myj  = json.load(page)
+        print "temp_c" , myj["current_observation"]["temp_c"] , '\n'
+        print "dewpoint_c" , myj["current_observation"]["dewpoint_c"] , '\n'
+        print "relative_humidity" , myj["current_observation"]["relative_humidity"] , '\n'
+        rettup.append(myj["current_observation"]["temp_c"])
+        rettup.append(myj["current_observation"]["dewpoint_c"])
+        rettup.append(myj["current_observation"]["relative_humidity"])
+    except:
+        pass
+    return rettup
+
+    
+if __name__ == "__main__":
+    while(1):
+        tup = get_report()
+        if len(tup) == 3:
+            print tup
+        time.sleep(5 * 60)
+    print 'Finished'
+    
